758e8df93664f10210cd0ec4b53e7bd1b288ff04
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2021-11-30 23:08:22 -0800

    Inserted complexify1 into functions.

--- fft.py
+++ fft.py
@@ -2,28 +2,66 @@
 import numpy as np
 from snowwhite.dftsolver import *
 from snowwhite.mddftsolver import *
+from fftx.utils import *
+# import time
 
-def fft(src):
-    problem = DftProblem(src.size, SW_FORWARD)
-    solver = DftSolver(problem)
-    result = solver.solve(src)
+_solver_cache = {}
+
+def _solver_key(tf, src, opts):
+    szstr = 'x'.join([str(n) for n in list(src.shape)])
+    retkey = tf + '_' + szstr
+    if opts.get(SW_OPT_CUDA, False):
+        retkey = retkey + '_CU'
+    return retkey
+    
+def fft(src, opts={}):
+    global _solver_cache
+    ckey = _solver_key('fft', src, opts)
+    solver = _solver_cache.get(ckey, 0)
+    if solver == 0:
+        problem = DftProblem(src.size, SW_FORWARD)
+        solver = DftSolver(problem)
+        _solver_cache[ckey] = solver
+    result = solver.solve(complexify1(src))
     return result
 
-def ifft(src):
-    problem = DftProblem(src.size, SW_INVERSE)
-    solver = DftSolver(problem)
-    result = solver.solve(src)
+def ifft(src, opts={}):
+    global _solver_cache
+    ckey = _solver_key('1fft', src, opts)
+    solver = _solver_cache.get(ckey, 0)
+    if solver == 0:
+        problem = DftProblem(src.size, SW_INVERSE)
+        solver = DftSolver(problem)
+        _solver_cache[ckey] = solver
+    result = solver.solve(complexify1(src))
     return result
 
 def fftn(src, opts={}):
-    problem = MddftProblem(list(src.shape), SW_FORWARD)
-    solver = MddftSolver(problem, opts)
-    result = solver.solve(src)
+    global _solver_cache
+    ckey = _solver_key('fftn', src, opts)
+    solver = _solver_cache.get(ckey, 0)
+    if solver == 0:
+        problem = MddftProblem(list(src.shape), SW_FORWARD)
+        solver = MddftSolver(problem, opts)
+        _solver_cache[ckey] = solver
+    # start_time = time.time()
+    result = solver.solve(complexify1(src))
+    # end_time = time.time()
+    # solve_time = end_time - start_time
+    # print(f"SOLVE fftn on {src.shape}: {solve_time}")
     return result
 
 def ifftn(src, opts={}):
-    problem = MddftProblem(list(src.shape), SW_INVERSE)
-    solver = MddftSolver(problem, opts)
-    result = solver.solve(src)
+    global _solver_cache
+    ckey = _solver_key('ifftn', src, opts)
+    solver = _solver_cache.get(ckey, 0)
+    if solver == 0:
+        problem = MddftProblem(list(src.shape), SW_INVERSE)
+        solver = MddftSolver(problem, opts)
+        _solver_cache[ckey] = solver
+    # start_time = time.time()
+    result = solver.solve(complexify1(src))
+    # end_time = time.time()
+    # solve_time = end_time - start_time
+    # print(f"SOLVE ifftn on {src.shape}: {solve_time}")
     return result
-
