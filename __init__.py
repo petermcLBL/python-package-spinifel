0d4cfed3ab91a112753400b3b2b06a3b1ddcba2a
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2023-09-14 00:09:14 -0400

    Import only utils.

--- __init__.py
+++ __init__.py
@@ -1,29 +1,22 @@
 """
-FFTX
-====
+SpiniFEL
+========
 
-This Python package provides NumPy/CuPy-compatible access to the 
-high performance multi-platform code generation capabilities of 
-the FFTX Project (https://github.com/spiral-software/fftx).  Both
-FFTX and this Python package originated under the Exascale Computing
+This Python package provides NumPy/CuPy-compatible utilities for
+the SpiniFEL code, and examples for use with FFTX.
+
+Both FFTX and this Python package originated under the Exascale Computing
 Project, with a focus on perforance-portable FFTs and FFT-using
 methods for the rapidly evolving high performance computing landscape.
 
 Modules
 -------
 
-fft
-    Compatible subset of NumPy and CuPy fft subpackages
-
-convo
-    Convolutions and convolution-like methods
+utils
+    Utilities for use with SpiniFEL
 """
 
-from . import fft
-
 from . import utils
-
-from . import convo
 
 __version__ = '1.0.1'
 
