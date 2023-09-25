595daef7a60901a0862c2d8f4efc16e430789669
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2023-09-25 17:52:20 -0400

    Change number of iterations from 105 to 15.

--- examples/run-kernel-tests-CPU.sh
+++ examples/run-kernel-tests-CPU.sh
@@ -2,8 +2,8 @@
 # source run-kernel-tests-CPU.sh > run-kernel-tests-CPU.out
 for sz in $(cat oddsizes.txt)
 do
-   python test-mdrfsconv.py $sz 105 5 d CPU
-   python test-mdrfsconv.py $sz 105 5 s CPU
-   python test-stepphase.py $sz 105 5 d CPU
-   python test-stepphase.py $sz 105 5 s CPU
+   python test-mdrfsconv.py $sz 15 5 d CPU
+   python test-mdrfsconv.py $sz 15 5 s CPU
+   python test-stepphase.py $sz 15 5 d CPU
+   python test-stepphase.py $sz 15 5 s CPU
 done
